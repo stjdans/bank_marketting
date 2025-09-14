8f63a4fd470416503839b778fd932ad34d13ef6e
stjdans <stjdans@naver.com>
2025-09-14 21:10:13 +0900

    메인페이지 디자인 2개 추가

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '초기화면'
+    return render_template('index.html')
+
+@app.route('/page1')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html')
 
 
 if __name__ == '__main__':
